b4e32df8f35f8afd79336319d097bcffc0957fb3
Megan Jane Thompson <59883929+megjthom@users.noreply.github.com>
2020-08-02 13:30:51 -0500

    Update PinWordEnumerator.java

--- PinWordEnumerator.java
+++ PinWordEnumerator.java
@@ -2,12 +2,9 @@ import java.util.Scanner;
 
 /** 
  * Title: Assignment 8 
- * Semester: COP3337 – Fall 2019
+ * Semester: COP3337 â€“ Fall 2019
  * @author Megan Jane Thompson
  *
- * I affirm that this program is entirely my own work
- * and none of it is the work of any other person.
- * 
  * This program converts a numeric pin to an equivalent word using the digit to 
  * letter mapping on a standard telephone keypad.
  */
